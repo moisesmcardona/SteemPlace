6d99f798af9bb8fad6ea06a4adafbcedd4e62691
moisesmcardona <moises@moises-studios.com>
2018-01-08 16:49:16 -0500

    MySQL Table Create Statement for the PHP SteemConnect Association

---
+++ SteemConnect User Association Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users2` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `drupalkey` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `approved` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
