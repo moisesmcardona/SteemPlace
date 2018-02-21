00548c9827406777c99cf22df0a57d9c50374c9f
moisesmcardona <moises@moises-studios.com>
2018-02-21 07:52:41 -0500

    Spanish community initiatives will be fetched directly from GitHub's spanishInitiatives.md file.

--- backend/showInitiatives.php
+++ backend/showInitiatives.php
@@ -3,8 +3,7 @@ function showIniciatives($language)
 {
     require_once 'Michelf/MarkdownExtra.inc.php';
     if ($language=="es") {
-        $spanishInitiativesFile = $_SERVER['DOCUMENT_ROOT'] . "/../files/steem.place/spanishInitiatives.md";
-        $body = fread(fopen($spanishInitiativesFile, "r"), filesize($spanishInitiativesFile));
+        $body = file_get_contents("https://raw.githubusercontent.com/moisesmcardona/SteemPlace/master/backend/spanishInitiatives.md");
     }
     echo \Michelf\MarkdownExtra::defaultTransform("$body");
 }
